e242c17ade125af748b91537df1222ac1ca74a11
billy.berkouwer <billy@ocove.studio>
2024-11-07 13:14:41 +0000

    fix: typeerror

--- src/app/(website)/api/node-mailer/route.ts
+++ src/app/(website)/api/node-mailer/route.ts
@@ -4,7 +4,7 @@ import nodemailer from "nodemailer";
 
 export const dynamic = "force-dynamic";
 export const fetchCache = 'force-no-store'
-export const maxDuration = "30"
+export const maxDuration = 30;
 export async function POST(req: Request, res: Response) {
   const formData = await req.formData();
   const email = formData.get("email") as string | undefined;
@@ -56,7 +56,7 @@ export async function POST(req: Request, res: Response) {
                 </html>`,
       };
 
-      const promise = new Promise(async (resolve, reject) => {
+      new Promise(async (resolve, reject) => {
         transporter.sendMail(mailOptions, (error: any, info: any) => {
           if (error) {
             console.log("Error:" + error);
@@ -72,8 +72,6 @@ export async function POST(req: Request, res: Response) {
           }
         });
       });
-
-      console.log(await promise)
     }
     return NextResponse.json({
       status: 500,
